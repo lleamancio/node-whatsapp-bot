46e11e581bf12d4faddb117e4d92c10c8889a5d8
Leticia Amancio <leticia.amancio@mercadolivre.com>
2023-02-23 22:45:19 -0300

    Adding implementation

---
+++ index.js
@@ -0,0 +1,30 @@
+const venom = require('venom-bot');
+
+venom
+  .create()
+  .then((client) => start(client));
+
+function start(client) {
+  client.onMessage((message) => {
+    if (message.body === 'Oi' || message.body === 'Olá') {
+      client
+        .sendText(message.from, 'Olá! Tudo bem com você?')
+        .then((result) => {
+          console.log('Result: ', result); //retorna um objeto de successo
+        })
+        .catch((erro) => {
+          console.error('Erro ao enviar mensagem: ', erro); //return um objeto de erro
+        });
+    }
+    if (message.body === 'Metodo Amancio' || message.body === 'Método Amâncio') {
+      client
+        .sendText(message.from, 'Olá DEV, sou uma mensagem automática, olha só, essa é um a solução rápida para um bot de whatsapp')
+        .then((result) => {
+          console.log('Result: ', result); //retorna um objeto de successo
+        })
+        .catch((erro) => {
+          console.error('Erro ao enviar mensagem: ', erro); //return um objeto de erro
+        });
+    }
+  });
+}
